4f6fcab044e34f210d47ec6e5e5b80c5b7a8bce2
Saroop <ssjagdev@outlook.com>
2025-09-25 17:27:01 +0100

    free cash flow, rev and net income graphs

--- fundamental_analysis.py
+++ fundamental_analysis.py
@@ -3,22 +3,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-ticker_symbol = "PLTR"
+ticker_symbol = "NESN.SW"
 
 ticker = yf.Ticker(ticker_symbol)
 income_statement = ticker.financials.T
 cashflow = ticker.cashflow.T
-print(cashflow.T.index)
 
-cashflow["FCF_calc"] = cashflow["Operating Cash Flow"] + cashflow["Capital Expenditure"]
-print(cashflow[["Free Cash Flow", "FCF_calc"]])
 
 
 print("----- Annual Income Statement -----")
-#print(income_statement)
+print(income_statement)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+ax[0].plot(income_statement.index, income_statement['Total Revenue'], marker='o', label='Total Revenue')
+ax[0].plot(income_statement.index, income_statement['Net Income'], marker='o', label='Net Income')
+ax[0].plot(cashflow.index, cashflow['Free Cash Flow'], marker='o', label='Free Cash Flow')
+ax[0].legend()
 
 
-plt.plot(income_statement.index, income_statement['Total Revenue'], marker='o', label='Total Revenue')
-plt.plot(income_statement.index, income_statement['Net Income'], marker='o', label='Net Income')
-
-#plt.show()
+plt.show()
