2a7403125a09695cc76e915aaecaee5781839347
Saroop <ssjagdev@outlook.com>
2025-09-25 21:55:29 +0100

    roe and roic graphs

--- fundamental_analysis.py
+++ fundamental_analysis.py
@@ -3,23 +3,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-ticker_symbol = "NESN.SW"
+ticker_symbol = "NVDA"
 
 ticker = yf.Ticker(ticker_symbol)
 income_statement = ticker.financials.T
 cashflow = ticker.cashflow.T
+balance_sheet = ticker.balance_sheet.T
 
 
 
-print("----- Annual Income Statement -----")
-print(income_statement)
 
-fig, ax = plt.subplots(figsize=(10, 6))
+fig, axes = plt.subplots(1,2,figsize=(10, 6))
 
-ax[0].plot(income_statement.index, income_statement['Total Revenue'], marker='o', label='Total Revenue')
-ax[0].plot(income_statement.index, income_statement['Net Income'], marker='o', label='Net Income')
-ax[0].plot(cashflow.index, cashflow['Free Cash Flow'], marker='o', label='Free Cash Flow')
-ax[0].legend()
+axes[0].plot(income_statement.index, income_statement['Total Revenue'], marker='o', label='Total Revenue')
+axes[0].plot(income_statement.index, income_statement['Net Income'], marker='o', label='Net Income')
+axes[0].plot(cashflow.index, cashflow['Free Cash Flow'], marker='o', label='Free Cash Flow')
+axes[0].legend()
 
-
+axes[1].plot(balance_sheet.index, income_statement['Net Income']/balance_sheet['Stockholders Equity'], marker='o', label='ROE')
+axes[1].plot(balance_sheet.index, income_statement['Operating Income']/(balance_sheet['Stockholders Equity'] + balance_sheet['Total Debt'] - balance_sheet['Cash Cash Equivalents And Short Term Investments']), marker='o', label='ROIC')
+axes[1].legend()
 plt.show()
