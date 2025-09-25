d20f41915895e7ff65fd5afb9843237a41639120
Saroop <ssjagdev@outlook.com>
2025-09-25 16:57:28 +0100

    plot rev and calc fcf

--- fundamental_analysis.py
+++ fundamental_analysis.py
@@ -1,1 +1,24 @@
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
+ticker_symbol = "PLTR"
+
+ticker = yf.Ticker(ticker_symbol)
+income_statement = ticker.financials.T
+cashflow = ticker.cashflow.T
+print(cashflow.T.index)
+
+cashflow["FCF_calc"] = cashflow["Operating Cash Flow"] + cashflow["Capital Expenditure"]
+print(cashflow[["Free Cash Flow", "FCF_calc"]])
+
+
+print("----- Annual Income Statement -----")
+#print(income_statement)
+
+
+plt.plot(income_statement.index, income_statement['Total Revenue'], marker='o', label='Total Revenue')
+plt.plot(income_statement.index, income_statement['Net Income'], marker='o', label='Net Income')
+
+#plt.show()
